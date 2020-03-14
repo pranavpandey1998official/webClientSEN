4d2d806b8de53a8c31cc9050d0e678cddc6ff75c
dkp1903 <dushyantpathak6789@gmail.com>
2020-03-14 14:37:34 +0530

    Added Search on Home and Bulma npm

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import Properties from './Properties';
+import Search from './Search';
+import Filter from './Filter';
 
 class Main extends Component
 {
     render()
     {
         return (
-                <div >
+            <div>
+                <div>
+                    <Filter />
+                    <Search />
+                </div>
+                <div>
                     <p>List of all properties</p>
                     <Properties />
-                    <div>
-                        Filter
-                    </div>
+                    
                 </div>
+            </div>
         );
     }
 }
