851ba959a7657834a348f754a893f6f86ae654bc
Dax Patel <32451078+Daxpatel1105@users.noreply.github.com>
2020-03-16 14:25:04 +0530

    Add files via upload

---
+++ src/components/Search/Searching.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Searching extends Component
+{
+    render()
+    {
+        return (
+                <div>Search</div>
+        );
+    }
+}
+
+export default Searching;
