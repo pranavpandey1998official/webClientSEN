483d4076058049e46cfa8090fa71c23ca499303d
dkp1903 <dushyantpathak6789@gmail.com>
2020-04-06 20:06:01 +0530

    Added Blog content

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import style from './style.css';
 
-class Blog extends Component {
-	render() {
-		return(
+class Blog extends Component 
+{
+	render() 
+	{
+		return
+		(
 			<div>
 				<h2 className='blogTitle' style={{style}}>
-					Is real estate worth investing in, today?
+					Real Estate investment in times of CoronaVirus
 				</h2>
 					
 				<p className='blogContent' style={{style}}>
-					The answer is yes; it is and always will be. <br />
-					An accurate figure states that there are almost 50000 births per day in India. 	
-				</p>
+					COVID-19 has shaken economies around the world to their roots. 
+					But how does it affect the Real Estate industry?
+
+					If you have already bought a house and were waiting for possession, get ready for a delay. <br />If you are planning to buy one, wait for things to settle down
+					It is difficult to think of a business that has not been impacted by the covid-19 pandemic. However, for the Indian real estate sector, the impact of the crisis can be a double whammy. The sector, especially the residential segment, has already been struggling with project delays, regulatory changes and low sales for the last few years.<br />
+
+The quantum of impact will depend on how long the lockdown will last and how long the economy will take to get back on track. Many experts said that the situation may deteriorate into a recession similar to what we saw in 2008, if not worse. Here are the possible outcomes for the real estate sector.
+<br />
+<b>Project delays</b> <br />
+If you are among those homebuyers who were expecting to get possession of your house anytime soon, be prepared to keep paying both the rent and equated monthly instalments (EMIs) for a few additional months, as project deliveries will get deferred. Given the lockdown, construction in incomplete projects has come to a complete standstill across the country.<br />
+
+“We foresee a delay of around seven to eight months in project completion on account of supply disruption due to the virus outbreak and the National Green Tribunal ban on construction last year across the national capital region (NCR)," said Amit Modi, director, ABA Corp., an NCR-based real estate developer.<br />
+
+The duration of project delays will not just depend on the lockdown. Even after the lockdown gets lifted, it will take time for developers to resume construction work in full swing as most of the labourers have left for their home towns. “With fear of the pandemic looming large, many may even opt to stay back in their villages longer. This will lead to shortage of labour and further project delays," said Anuj Puri, chairman, ANAROCK Property Consultants. As per ANAROCK research, as many as 156.2 million housing units are currently under construction across the top seven cities in India, of which nearly 46.6 million units were to be delivered in 2020.<br />
+<b>
+Demand-supply disruption </b><br />
+During times of uncertainty, most people defer plans to make big-ticket purchases. As a result, home sales are bound to fall. “Housing sales in the first quarter of 2020 plunged by 42% over the corresponding period in 2019," said Puri.
+<br />
+Developers, typically, schedule project launches during festivals such as Gudi Padwa, Akshaya Tritiya, Navratri and Ugadi, which fall in March-April. But given the ongoing lockdown due to the pandemic, most of them have deferred their plans this year.
+<br />
+<b>
+Price reactions </b>
+Most experts and developers believe that prices will largely remain stagnant though the costs may go up. “There will be no change in prices as transactions are not taking place. The prices will resume at the same level," said Mohit Goel, CEO, Omaxe Ltd, a real estate developer. Niranjan Hiranandani, national president, National Real Estate Development Council (Naredco), a real estate developers’ body, agreed. “In the primary market, I do not foresee any major changes. The market scenario has been ‘challenging’ for some time now, and possible discounts have already been factored into the current pricing. I do not see the option for any further rate reduction," he said.
+<br />
+Developers anticipate increase in raw material cost, but they may not be able to pass that on to buyers in the current market situation. “India is a big importer of steel and iron products, technical construction equipment as well as plastic and fibre elements from China. With production in China going down, the prices of these materials may rise, increasing costs and reducing profit margins for developers in India," said Vikas Jain, CEO, Labdhi Lifestyle, a Mumbai-based real estate developer.
+
+Meanwhile, the commercial space may suffer for a while. According to a press release by ICRA on the impact of coronavirus, the retail commercial space comprising mainly of mall operators will be impacted in a major way, as a result of closures across the country, but the repercussions on office spaces are likely to be marginal.
+
+There may be outliers though. “There is always the possibility that a few cash-starved developers with high unsold stock may sweeten deals for buyers. But this was happening even before the pandemic," said Puri.
+</p>
 			</div>
 		)
 	}
