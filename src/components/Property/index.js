76a45c4a57936d5417992be1f0e4ef834773dc8e
Pranav Pandey <pranavpandey1998official@gmail.com>
2020-04-06 13:01:42 +0530

    fixed slider

--- src/components/Property/index.js
+++ src/components/Property/index.js
@@ -4,10 +4,6 @@ import { withRouter } from 'react-router-dom';
 import { SERVER_URL } from '../../utils/constants';
 import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
 
-
-
-
-
 class Extended extends Component {
   
   constructor(props){
@@ -17,93 +13,66 @@ class Extended extends Component {
     }
   }
 
-  componentDidMount() {
-    var carousels = bulmaCarousel.attach('.carousel', {
-      slidesToScroll: 1,
-      slidesToShow: 2,
-      loop: true
-    });
-    
-  }
-
   async componentDidMount() {
-    
+    const carousels = bulmaCarousel.attach('#carousel-demo', {
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      infinite: true
+    });
     const propertyId= this.props.match.params.id;
     const url = SERVER_URL + '/property/'+propertyId;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    this.setState({property: data.property});
+    this.setState({property: data.property})
   }
   
   render() {
     return (
-      <div class="container" >
+      <div className="container" >
        
-        <div class="block">
-          <h1 class="title is-4" style={{ paddingLeft: 40 }}>
+        <div className="block">
+          <h1 className="title is-4" style={{ paddingLeft: 40 }}>
             Shiva Bungalows</h1>
             {/* sample here */}
             <i className="fas fa-building" />
         </div>
         
-       
-        <div id="carousel" class='carousel carousel-animated carousel-animate-slide' style={{overflow:"hidden"}}>
-  <div class='carousel-container'>
-    <div class='carousel-item has-background is-active'>
-      <img class="is-background" src="https://wikiki.github.io/images/merry-christmas.jpg" alt="" width="640" height="310" />
-      <div class="title">Merry Christmas</div>
-    </div>
-    <div class='carousel-item has-background'>
-      <img class="is-background" src="https://wikiki.github.io/images/singer.jpg" alt="" width="640" height="310" />
-      <div class="title">Original Gift: Offer a song with <a href="https://lasongbox.com" target="_blank">La Song Box</a></div>
-    </div>
-    <div class='carousel-item has-background'>
-      <img class="is-background" src="https://wikiki.github.io/images/sushi.jpg" alt="" width="640" height="310" />
-      <div class="title">Sushi time</div>
-    </div>
-    <div class='carousel-item has-background'>
-      <img class="is-background" src="https://wikiki.github.io/images/life.jpg" alt="" width="640" height="310" />
-      <div class="title">Life</div>
-    </div>
-  </div>
-  <div class="carousel-navigation">
-    <div class="carousel-nav-left">
-      <i class="fa fa-chevron-left" aria-hidden="true"></i>
-    </div>
-    <div class="carousel-nav-right">
-      <i class="fa fa-chevron-right" aria-hidden="true"></i>
-    </div>
-  </div>
-</div>
 
-
-
-        <div class='card-content is-flex' style={{ justifyContent: "center" }}>
-          <figure class="image is-5x4 " >
-            <img style={{ height: 300, width: 300 }} src="http://www.hdnicewallpapers.com/Walls/Big/House%20and%20Bungalow/Fabulous_Unique_Home_HD_Wallpapers.jpg" alt="Image" />
-          </figure>
+        <div id="carousel-demo" class="carousel has-text-centered">
+          <div class="item-1">
+          <img className="is-background is-centered" src="https://wikiki.github.io/images/singer.jpg" alt=""  />
+            
+          </div>
+          <div class="item-2">
+          <img className="is-background is-centered" src="https://wikiki.github.io/images/singer.jpg" alt="" />
+            
+          </div>
+          <div class="item-3">
+          <img className="is-background is-centered" src="https://wikiki.github.io/images/singer.jpg" alt=""/>
+            
+          </div>
         </div>
 
 
 
-        <div class="block">
-          <nav class="navbar">
-            <div class="nav-menu is-active">
-              <a href="#Description" class="nav-item is-tab" style={{ padding: 20 }}>DESCRIPTION</a>
-              <a href="#Details" class="nav-item is-tab" style={{ padding: 20 }}>DETAILS</a>
-              <a href="#Facilities" class="nav-item is-tab" style={{ padding: 20 }}>FACILITIES</a>
-              <a href="#Location" class="nav-item is-tab" style={{ padding: 20 }}>LOCATION</a>
-              <a href="#Reviews" class="nav-item is-tab" style={{ padding: 20 }}>REVIEWS</a>
+        <div className="block">
+          <nav className="navbar">
+            <div className="nav-menu is-active">
+              <a href="#Description" className="nav-item is-tab" style={{ padding: 20 }}>DESCRIPTION</a>
+              <a href="#Details" className="nav-item is-tab" style={{ padding: 20 }}>DETAILS</a>
+              <a href="#Facilities" className="nav-item is-tab" style={{ padding: 20 }}>FACILITIES</a>
+              <a href="#Location" className="nav-item is-tab" style={{ padding: 20 }}>LOCATION</a>
+              <a href="#Reviews" className="nav-item is-tab" style={{ padding: 20 }}>REVIEWS</a>
 
             </div>
           </nav>
         </div>
-        <div class="block">
-          <section id="Description" class="hero  is-light ">
-            <div class="hero-body">
+        <div className="block">
+          <section id="Description" className="hero  is-light ">
+            <div className="hero-body">
 
-              <h1 class="title" style={{ MarginTop: "0px" }}>
+              <h1 className="title" style={{ MarginTop: "0px" }}>
                 Description
       </h1>
               <p>
@@ -112,30 +81,30 @@ class Extended extends Component {
 
             </div>
           </section>
-          <section id="Details" class="hero  is-light ">
-            <div class="hero-body">
+          <section id="Details" className="hero  is-light ">
+            <div className="hero-body">
 
-              <h1 class="title">
+              <h1 className="title">
                 Details
       </h1>
-              <div class='container'>
-                <div class="content">
-                  <div class="level">
-                    <div class="level-left">
+              <div className='container'>
+                <div className="content">
+                  <div className="level">
+                    <div className="level-left">
                       <ul>
                         <li><strong>Plot Area:</strong> 500 sq.ft </li>
                         <li><strong>Bathrooms:</strong> 3</li>
                         <li><strong>Bedrooms:</strong> 2</li>
                       </ul>
                     </div>
-                    <div class="level-center">
+                    <div className="level-center">
                       <ul>
                         <li><strong>Property Age:</strong> 5 year(s)</li>
                         <li><strong>Price:</strong> 15,000</li>
                         <li><strong>Pincode:</strong> 382007</li>
                       </ul>
                     </div>
-                    <div class="level-right">
+                    <div className="level-right">
                       <ul>
                         <li><strong>City:</strong> Ahmedabad</li>
                         <li><strong>Property Owner:</strong> SEN</li>
@@ -148,23 +117,23 @@ class Extended extends Component {
 
             </div>
           </section>
-          <section id="Facilties" class="hero  is-light ">
-            <div class="hero-body">
+          <section id="Facilties" className="hero  is-light ">
+            <div className="hero-body">
 
-              <h1 class="title" style={{ MarginTop: "0px" }}>
+              <h1 className="title" style={{ MarginTop: "0px" }}>
                 Aminent Facilities
       </h1>
 
 
             </div>
           </section>
-          <section id="Location" class="hero  is-light ">
-            <div class="hero-body">
+          <section id="Location" className="hero  is-light ">
+            <div className="hero-body">
 
-              <h1 class="title">
+              <h1 className="title">
                 Location
       </h1>
-              <div class='card-content is-flex' style={{ justifyContent: "center" }}>
+              <div className='card-content is-flex' style={{ justifyContent: "center" }}>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3667.497563754517!2d72.62677371497078!3d23.188530984869374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c2a3c9618d2c5%3A0xc54de484f986b1fa!2sDA-IICT!5e0!3m2!1sen!2sin!4v1586021912902!5m2!1sen!2sin" style={{ width: "300", height: "350" }}></iframe>
 
               </div>
