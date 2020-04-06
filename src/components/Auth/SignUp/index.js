a5e69ba78c4ff9e4f8e39edd81e609657b000d7f
Pranav Pandey <pranavpandey1998official@gmail.com>
2020-04-06 11:02:26 +0530

    fixed bugs in auth

--- src/components/Auth/SignUp/index.js
+++ src/components/Auth/SignUp/index.js
@@ -7,13 +7,14 @@ import { createUser } from '../../../actions/auth';
 
 class NormalSignUpForm extends React.Component {
 	render() {
+		const { errors, touched } = this.props
 		return (
 			<div className="container">
 				<div className="columns is-centered">
 					<div className="column is-half">
 						<div className="card">
-							<header class="card-header">
-								<h1 class="card-header-title is-size-2">
+							<header className="card-header">
+								<h1 className="card-header-title is-size-2">
 									Sign Up
 								</h1>
 							</header>
@@ -21,39 +22,44 @@ class NormalSignUpForm extends React.Component {
 
 								<div className="content">
 									<Form>
-										<div class="field">
-											<label class="label">First Name</label>
-											<div class="control">
-												<Field class="input" name="firstName" type="text" placeholder="First Name" />
+										<div className="field">
+											<label className="label">First Name</label>
+											<div className="control">
+												<Field className="input" name="firstName" type="text" placeholder="First Name" />
 											</div>
+											{touched.firstName && errors.firstName ? <p class="help is-danger">{errors.firstName}</p> : null }
 										</div>
-										<div class="field">
-											<label class="label">Last Name</label>
-											<div class="control">
-												<Field class="input" name="lastName" type="text" placeholder="Last Name" />
+										<div className="field">
+											<label className="label">Last Name</label>
+											<div className="control">
+												<Field className="input" name="lastName" type="text" placeholder="Last Name" />
 											</div>
+											{touched.lastName && errors.lastName ? <p class="help is-danger">{errors.lastName}</p> : null }
 										</div>
-										<div class="field">
-											<label class="label">Email</label>
-											<div class="control">
-												<Field class="input" name="email" type="text" placeholder="Email" />
+										<div className="field">
+											<label className="label">Email</label>
+											<div className="control">
+												<Field className="input" name="email" type="text" placeholder="Email" />
 											</div>
+											{touched.email && errors.email ? <p class="help is-danger">{errors.email}</p> : null }
 										</div>
-										<div class="field">
-											<label class="label">Password</label>
-											<div class="control">
-												<Field class="input" name="password" type="text" placeholder="Password" />
+										<div className="field">
+											<label className="label">Password</label>
+											<div className="control">
+												<Field className="input" name="password" type="password" placeholder="Password" />
 											</div>
+											{touched.password && errors.password ? <p class="help is-danger">{errors.password}</p> : null }
 										</div>
-										<div class="field">
-											<label class="label">Confirm Password</label>
-											<div class="control">
-												<Field class="input" name="confirmPassword" type="text" placeholder="Re-type Password" />
+										<div className="field">
+											<label className="label">Confirm Password</label>
+											<div className="control">
+												<Field className="input" name="confirmPassword" type="password" placeholder="Re-type Password" />
 											</div>
+											{touched.confirmPassword && errors.confirmPassword ? <p class="help is-danger">{errors.confirmPassword}</p> : null }
 										</div>
-										<div class="field">
-											<div class="control is-centered">
-												<button class="button is-success is-centered" type="submit">
+										<div className="field">
+											<div className="control is-centered">
+												<button className="button is-success is-centered" type="submit">
 													Submit
 												</button>
 											</div>
@@ -85,9 +91,9 @@ const SignUp = withFormik({
 	},
 	validationSchema: Yup.object().shape({
 		email: Yup.string().email('Please enter a valid email').required(),
-		password: Yup.string().required(),
-		firstName: Yup.string().required(),
-		lastName: Yup.string().required(),
+		password: Yup.string().required('password cannot be empty'),
+		firstName: Yup.string().required('first name cannot be empty'),
+		lastName: Yup.string().required('last name cannot be empty'),
 		confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
 	})
 })(NormalSignUpForm)
