7c6975c78cea4627a1bad3f7d5632fa2c5dd629a
unknown <sagar.sind20@gmail.com>
2020-04-09 11:35:00 +0530

    modified test for signup

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -27,15 +27,15 @@ describe('Test For Sign Up Page', function() {
                 cy.get('.help.is-danger').eq(3).invoke('text')
                 .then((text)=>{
                 const req = text;
-                expect(req).to.equal("password cannot be empty");})
+                expect(req).to.equal("Please Enter your password");})
                 cy.url().should('include', '/signUp')
             });
             it('test signUp ivalid email',function(){
                 cy.get('input[name="firstName"]').type('Sagar').should('have.value', 'Sagar')
                 cy.get('input[name="lastName"]').type('Singh').should('have.value', 'Singh')
                 cy.get('input[name="email"]').type('sagar.sind20gmail.com').should('have.value', 'sagar.sind20gmail.com')
-                cy.get('input[name="password"]').type('lol').should('have.value', 'lol')
-                cy.get('input[name="confirmPassword"]').type('lol').should('have.value', 'lol')
+                cy.get('input[name="password"]').type('A123.a123').should('have.value', 'A123.a123')
+                cy.get('input[name="confirmPassword"]').type('A123.a123').should('have.value', 'A123.a123')
                 cy.get('.button.is-success').contains("Submit").click()
                 cy.get('.help.is-danger').eq(0).invoke('text')
                 .then((text)=>{
@@ -47,13 +47,26 @@ describe('Test For Sign Up Page', function() {
                 cy.get('input[name="firstName"]').type('Sagar').should('have.value', 'Sagar')
                 cy.get('input[name="lastName"]').type('Singh').should('have.value', 'Singh')
                 cy.get('input[name="email"]').type('sagar.sind20@gmail.com').should('have.value', 'sagar.sind20@gmail.com')
-                cy.get('input[name="password"]').type('lol').should('have.value', 'lol')
+                cy.get('input[name="password"]').type('A123.a123').should('have.value', 'A123.a123')
+                cy.get('input[name="confirmPassword"]').type('A123.a234').should('have.value', 'A123.a234')
+                cy.get('.button.is-success').contains("Submit").click()
+                cy.get('.help.is-danger').eq(1).invoke('text')
+                .then((text)=>{
+                const req = text;
+                expect(req).to.equal("Passwords must match");})
+                cy.url().should('include', '/signUp')
+            });
+            it('test signUp not strong password',function(){
+                cy.get('input[name="firstName"]').type('Sagar').should('have.value', 'Sagar')
+                cy.get('input[name="lastName"]').type('Singh').should('have.value', 'Singh')
+                cy.get('input[name="email"]').type('sagar.sind20@gmail.com').should('have.value', 'sagar.sind20@gmail.com')
+                cy.get('input[name="password"]').type('123').should('have.value', '123')
                 cy.get('input[name="confirmPassword"]').type('123').should('have.value', '123')
                 cy.get('.button.is-success').contains("Submit").click()
                 cy.get('.help.is-danger').eq(0).invoke('text')
                 .then((text)=>{
                 const req = text;
-                expect(req).to.equal("Passwords must match");})
+                expect(req).to.equal("Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character");})
                 cy.url().should('include', '/signUp')
             });
             it('test SignUp already registered email',function(){
@@ -61,8 +74,8 @@ describe('Test For Sign Up Page', function() {
                 cy.get('input[name="firstName"]').type('Sagar').should('have.value', 'Sagar')
                 cy.get('input[name="lastName"]').type('Singh').should('have.value', 'Singh')
                 cy.get('input[name="email"]').type('manupanday1998@gmail.com').should('have.value', 'manupanday1998@gmail.com')
-                cy.get('input[name="password"]').type('lol').should('have.value', 'lol')
-                cy.get('input[name="confirmPassword"]').type('lol').should('have.value', 'lol')
+                cy.get('input[name="password"]').type('123456').should('have.value', '123456')
+                cy.get('input[name="confirmPassword"]').type('123456').should('have.value', '123456')
                 cy.get('.button.is-success').contains("Submit").click()
                 cy.wait(1000)
                 cy.get('.Toastify__toast-body').invoke('text')
